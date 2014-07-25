659c22894d74fa074da360f52611552b1c28bec0
ncmochacity <nickichenbattle@gmail.commkdir ~/myprojectgit config --global user.email nickichenbattle@gmail.com>
2014-07-25 03:30:26 -0500

    cart

---
+++ cart/cart.php
@@ -0,0 +1,48 @@
+<?php
+function add_item($key, $quantity){
+	global $products;
+	if ($quantity < 1) return;
+
+	if (isset($_SESSION['cart12'][$key])){
+		$quantity +=$_SESSION[$key]['qty'];
+		update_item($key, $quantity);
+		return;
+	}
+	$cost= $products[$key]['cost'];
+	$total =$cost * $quantity;
+	$item= array(
+			'name' => $products[$key]['name'];
+			'cost' => $cost,
+			'qty'  => $quantity,
+			'total' => $total
+
+
+
+		);
+	$_SESSION['cart12'][$key]=$item;
+}
+function update_item($key, $quantity){
+	global $products;
+	$quantity= (int) $quantity;
+	if (isset($_SESSION['cart12'][$key])){
+		if($quantity <=0) {
+			unset($_SESSION['cart12'][$key]);
+		}
+		else{
+			$_SESSION['cart12'][$key]['qty']=$quantity;
+			$total=$_SESSION['cart12'][$key]['cost'] *
+					$_SESSION['cart12'][$key]['qty'];
+			$_SESSION['cart12'][$key]['total']=$total;
+		}
+	}
+}
+function get_subtotal(){
+	$subtotal=0;
+	foreach ($_SESSION['cart12'] as $item){
+		$subtotal +=$item['total'];
+	}
+	$subtotal=number_format($subtotal, 2);
+	return $subtotal;
+}
+
+?>
