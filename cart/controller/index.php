ea0db82dd1ada2ea9b159fe3e28593651be0a11a
ncmochacity <nickichenbattle@gmail.commkdir ~/myprojectgit config --global user.email nickichenbattle@gmail.com>
2014-08-12 03:10:45 -0500

    sign up

---
+++ cart/controller/index.php
@@ -0,0 +1,44 @@
+<?php
+//error_reporting(E_ALL);
+//ini_set("display_errors", "On");
+
+require('../model/textfields.php');
+require('../model/validate.php');
+
+global $validate, $fields, $first_name, $last_name, $phone, $email;
+
+$validate = new Validate();
+$fields = $validate->getFields();
+$fields->addField('first_name');
+$fields->addField('last_name');
+$fields->addField('phone', 'Use 000-111-333 format. ');
+$fields->addField('email', 'Must be a valid email address. ');
+
+if (isset($_POST['action'])) {
+    $action = $_POST['action'];
+} else {
+    $action = 'reset';
+}
+$action = strtolower($action);
+switch ($action) {
+    case 'reset':
+        include('../view/register.php');
+        break;
+    case 'register':
+        $first_name = trim($_POST['first_name']);
+        $last_name = trim($_POST['last_name']);
+        $phone = trim($_POST['phone']);
+        $email = trim($_POST['email']);
+
+        $validate->text('first_name', $first_name);
+        $validate->text('last_name', $last_name);
+        $validate->phone('phone', $phone);
+        $validate->email('email', $email);
+
+        if ($fields->hasErrors()) {
+            include('../view/register.php');
+        } else {
+            include('../view/success.php');
+        }
+}
+?>
