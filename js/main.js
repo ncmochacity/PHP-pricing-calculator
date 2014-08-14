4d81eb96c6c9a896c87a862c1256771881de0bb0
ncmochacity <nickichenbattle@gmail.commkdir ~/myprojectgit config --global user.email nickichenbattle@gmail.com>
2014-08-14 01:14:28 -0500

    PHP project

--- js/main.js
+++ js/main.js
@@ -46,5 +46,38 @@ $(document).ready(function() {
         }
         return [];
     }
-
+    
+    
+        jQuery(window).scroll(function() {
+            if (jQuery(this).scrollTop() > 1000) {
+                jQuery('.back-to-top').fadeIn();
+            } else {
+                jQuery('.back-to-top').fadeOut();
+            }
+        });
+        jQuery('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, 800);
+        return false;
+        })
+    
+    
+    
+    
+    var offset=0;
+    var duration=500;
+    jQuery(window).scroll(function(){
+        if(jquery(this).scrollTop() > offset){
+            jquery("#top").fadeIn(duration);
+        }
+        else{
+            jquery("#top").fadeOut(duration);
+        }
+    });
+    jQuery("#top").click(function(event){
+        event.preventDefault();
+        jQuery('html, body').animate({
+            scrollTop: 700
+        }, duration);
+    })
 });
